b5f4ee975309f379899d6badd2920b339e2b409f
Anurag Tiwari <37267297+atzones@users.noreply.github.com>
2018-04-30 00:48:32 +0530

    Create axd1.js

---
+++ axd1.js
@@ -0,0 +1,17 @@
+     var snParent = document.getElementByIdundefined"axdsense1");
+
+
+  var parent_width = parseIntundefinedsnParent.offsetWidth);
+  var parent_height = 210;
+  if undefinedparent_width >= 1200) { parent_width = 1200; }
+  if undefinedparent_width <= 500) { parent_height = 100; }
+  var ins = document.createElementundefined"ins");
+  ins.innerHTML = '<ins class="adsbygoogle"' +
+  'style="display:inline-block;width:' + parent_width + 'px;height:' + parent_height + 'px"' +
+  ' data-ad-client="ca-pub-2211366945815495"' +
+  ' data-ad-slot="8750142937"' +
+  ' data-font-face="verdana"' +
+  ' data-max-num-ads="3">' +
+  '';
+  snParent.appendChildundefinedins);
+  undefinedadsbygoogle=window.adsbygoogle || []).pushundefined{});
